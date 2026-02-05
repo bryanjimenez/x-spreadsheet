ac0fdd7d764ecd74bb2d58c08d0844c250581350
Bryan <bryanjimenez6@gmail.com>
2026-02-04 20:45:35 -0500

    v0.1.7 (#11)
    FIX: getExportData cells.text as string b25d1d4
    FIX: fix edge cases w/ formulas + test e083d21
    remove unused + .gitignore update 896f994
    DEP: rspack + eslint + prettier 8e5be78

--- src/core/formula.ts
+++ src/core/formula.ts
@@ -1,6 +1,12 @@
 import md5 from "md5";
 import { tf } from "../locale/locale";
-import { numberCalc } from "./helper";
+import {
+  isBoolean,
+  isBooleanLoose,
+  isBooleanStrict,
+  isNumber,
+  numberCalc,
+} from "./helper";
 
 // TODO: map key and render types
 export interface Formula {
@@ -14,39 +20,106 @@ export const baseFormulas: Formula[] = [
   {
     key: "SUM",
     title: tf("formula.sum"),
-    // FIXME: numberCalc should probably throw
-    render: (ary: number[]) => ary.reduce((a, b) => numberCalc("+", a, b), 0),
+    render: (ary: number[]) =>
+      ary.reduce((a, b) => {
+        if (a === "#VALUE!" || !isNumber(b)) {
+          return "#VALUE!";
+        }
+        return String(numberCalc("+", a, b));
+      }, "0"),
   },
   {
     key: "AVERAGE",
     title: tf("formula.average"),
-    render: (ary: string[]) =>
-      ary.reduce((a, b) => Number(a) + Number(b), 0) / ary.length,
+    render: (ary: string[]) => {
+      if (ary.length === 0) {
+        return "#DIV/0!";
+      }
+
+      const sum = ary.reduce((a, b) => {
+        if (a === "#VALUE!" || !isNumber(b)) {
+          return "#VALUE!";
+        }
+        return String(numberCalc("+", a, b));
+      }, "0");
+
+      if (sum === "#VALUE!") {
+        return "#VALUE!";
+      }
+
+      return Number(sum) / ary.length;
+    },
   },
   {
     key: "MAX",
     title: tf("formula.max"),
-    render: (ary: string[]) => Math.max(...ary.map((v) => Number(v))),
+    render: (ary: string[]) => {
+      if (ary.length === 0) {
+        return "#VALUE!";
+      }
+
+      return Math.max(
+        ...(ary.length < 2 ? [0] : []),
+        ...ary.reduce<number[]>(
+          (acc, v) => (!isNumber(v) ? acc : [...acc, Number(v)]),
+          []
+        )
+      );
+    },
   },
   {
     key: "MIN",
     title: tf("formula.min"),
-    render: (ary: string[]) => Math.min(...ary.map((v) => Number(v))),
+    render: (ary: string[]) => {
+      if (ary.length === 0) {
+        return "#VALUE!";
+      }
+
+      return Math.min(
+        ...(ary.length < 2 ? [0] : []),
+        ...ary.reduce<number[]>(
+          (acc, v) => (!isNumber(v) ? acc : [...acc, Number(v)]),
+          []
+        )
+      );
+    },
   },
   {
     key: "IF",
     title: tf("formula._if"),
-    render: ([b, t, f]: [boolean, unknown, unknown]) => (b ? t : f),
+    render: ([b, t, f]: [boolean, unknown, unknown]) => {
+      if (b === undefined) {
+        return "#VALUE!";
+      }
+
+      if (typeof b === "boolean" || isBoolean(b)) {
+        return isBooleanStrict(b) ? t : f;
+      }
+      return "#VALUE!";
+    },
   },
   {
     key: "AND",
     title: tf("formula.and"),
-    render: (ary: string[]) => ary.every((it) => it),
+    render: (ary: string[]) => {
+      if (ary.length === 0) {
+        return "#VALUE!";
+      }
+      return ary.reduce((a, b) => isBooleanLoose(a) && isBooleanLoose(b), true);
+    },
   },
   {
     key: "OR",
     title: tf("formula.or"),
-    render: (ary: string[]) => ary.some((it) => it),
+    render: (ary: string[]) => {
+      if (ary.length === 0) {
+        return "#VALUE!";
+      }
+      return ary.reduce(
+        (a, b) => isBooleanLoose(a) || isBooleanLoose(b),
+        false
+      );
+    },
   },
   {
     key: "CONCAT",
